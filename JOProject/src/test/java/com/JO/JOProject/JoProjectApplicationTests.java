623f41bb7e2e33a6dd8ce4356d182e214c6ab174
mathys muganga <147081509+forSplinter@users.noreply.github.com>
2024-05-26 13:40:45 +0200

    v1.0

---
+++ JOProject/src/test/java/com/JO/JOProject/JoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JO.JOProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
